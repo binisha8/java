a231233539ab31e492a5ab12f7c18f6efc87f793
Binisha Maharjan <115027066+binisha8@users.noreply.github.com>
2025-10-19 18:05:44 +0545

    Add files via upload

---
+++ logexpo.java
@@ -0,0 +1,20 @@
+public class logexpo {
+   
+        public static void main(String[] args){
+            double E =Math.E;
+            System.out.println(E);
+            double x= Math.log(E);
+            double y = Math.log10(100);
+
+
+            System.out.println(x);
+            System.out.println(y);
+
+            double a= Math.pow(2,3);
+            System.out.println(a);
+
+
+        }
+    
+    
+}
