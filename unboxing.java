b8fbdbb4f813eeee4895be3d159ad3d6419c9a68
Binisha Maharjan <115027066+binisha8@users.noreply.github.com>
2025-10-19 18:06:21 +0545

    Add files via upload

---
+++ unboxing.java
@@ -0,0 +1,23 @@
+public class unboxing {
+    public static void main(String[] args)
+    {
+         Integer  a=100;
+         Double b=90.987;
+         Character c='h';
+
+     // unboxing
+
+         int x=a;
+         double y=b;
+         char z=c;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+
+
+
+    }
+    
+}
