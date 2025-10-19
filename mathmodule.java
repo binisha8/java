73885a971d0dd6425488685241f0d5172511ee7d
Binisha Maharjan <115027066+binisha8@users.noreply.github.com>
2025-10-19 18:07:38 +0545

    Add files via upload

---
+++ mathmodule.java
@@ -0,0 +1,9 @@
+public class mathmodule {
+    public static void main(String[] args) {
+        double maxval = Math.max(2.2,8.0);
+        double minval = Math.min(2.2,8.0);
+
+        System.out.println(maxval);
+        System.out.println(minval);
+    }
+}
