3782ee84ee192b80bcd9842459b68d96dcf2f1c9
Binisha Maharjan <115027066+binisha8@users.noreply.github.com>
2025-10-19 18:08:05 +0545

    Add files via upload

---
+++ functions2.java
@@ -0,0 +1,11 @@
+
+class Main{
+    static int squre(int num){
+        return num*num;
+    }
+    public static void main(String[] args)
+    {
+        System.out.println((square(num:10));
+        System.out.println((square(num:7));  
+    }
+}
